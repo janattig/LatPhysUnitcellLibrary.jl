10b8b4e4e9a47588cc997245dc76cba805ec416c
Jan Attig <jattig@thp.Uni-Koeln.DE>
2018-11-15 12:41:58 +0100

    added code from main project

--- src/LatPhysUnitcellLibrary.jl
+++ src/LatPhysUnitcellLibrary.jl
@@ -1,5 +1,41 @@
+################################################################################
+#
+#   module LatPhysUnitcellLibrary
+#   -> LatticePhysics_Base
+#   -> LinearAlgebra
+#
+#   --> PRE-IMPLEMENTED UNITCELLS 2D
+#
+#   --> PRE-IMPLEMENTED UNITCELLS 3D
+#
+################################################################################
+
+# TODO LIST
+# --> TODO common interface getUnitcell(:square)
+# --> TODO version vs. implementation
+# --> TODO UnitcellDefinitions -> UnitcellLibrary
+
+
+
+
+
+# module start
 module LatPhysUnitcellLibrary
 
-greet() = print("Hello World!")
 
-end # module
+
+# include interface
+include("interface.jl")
+
+
+
+# include 2d stuff
+include("definitions_2d.jl")
+
+# include 3d stuff
+include("definitions_3d.jl")
+
+
+
+# module end
+end
---
+++ src/definitions_2d.jl
@@ -0,0 +1,29 @@
+################################################################################
+#
+#   DEFINITION OF 2D UNITCELLS
+#
+#   Code is included in subfiles
+#
+################################################################################
+
+
+# SQUARE / RECTANGULAR LIKE
+
+# Square lattice
+include("unitcells_2d/square.jl")
+
+
+
+
+
+
+# TRIANGULAR LIKE
+
+# Triangular lattice
+include("unitcells_2d/triangular.jl")
+
+# Honeycomb lattice
+include("unitcells_2d/honeycomb.jl")
+
+# Kagome lattice
+include("unitcells_2d/kagome.jl")
