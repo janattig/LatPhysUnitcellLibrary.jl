9be81ca4f44844b9fabec73b49a27b69fd19bdfc
Jan <jattig@thp.uni-koeln.de>
2018-11-30 14:36:26 +0100

    included files for tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,10 +4,13 @@ using Test
 @testset "LatPhysUnitcellLibrary.jl" begin
 
     # TEST THE 2D UNITCELLS
+    include("tests_2d_unitcells.jl")
 
     # TEST THE 3D UNITCELLS
+    include("tests_3d_unitcells.jl")
 
     # TEST THE CODE GENERATION
+    include("tests_code_generation.jl")
 
-    
+
 end
