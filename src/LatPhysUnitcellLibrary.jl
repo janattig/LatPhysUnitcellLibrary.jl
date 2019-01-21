070d8a48bb2b19a42bd8cfdcdc72d9bea718d0a9
Jan Attig <jattig@thp.Uni-Koeln.DE>
2019-01-21 14:51:00 +0100

    changed file structure to plan for automatic code generation

--- src/LatPhysUnitcellLibrary.jl
+++ src/LatPhysUnitcellLibrary.jl
@@ -25,8 +25,8 @@ using LatPhysBase
 # include interface
 include("interface.jl")
 
-# include code generation for unitcell files
-include("code_generation.jl")
+# include code generation tools for unitcell files
+include("code_generation_tools.jl")
 
 
 
@@ -36,6 +36,9 @@ include("definitions_2d.jl")
 # include 3d stuff
 include("definitions_3d.jl")
 
+
+# include the generation of wrapper functions of code
+include("code_generation.jl")
 
 
 # module end
