d624abc5cf72b04d441419fd654dda716be465d6
Jan Attig <jattig@thp.Uni-Koeln.DE>
2019-02-05 12:02:35 +0100

    implemented pi flux square lattice implementation

--- src/unitcells_2d/square.jl
+++ src/unitcells_2d/square.jl
@@ -89,3 +89,53 @@ function getUnitcellSquare(
         ]
     )
 end
+
+
+# Implementation
+# - implementation -3 (same as 3 but with pi flux)
+# - labels <: Any
+function getUnitcellSquare(
+            unitcell_type  :: Type{U},
+            implementation :: Val{-3}
+        ) :: U where {LS,LB,S<:AbstractSite{LS,2},B<:AbstractBond{LB,2},U<:AbstractUnitcell{S,B}}
+
+    # return a new Unitcell
+    return newUnitcell(
+        # Type of the unitcell
+        U,
+        # lattice vectors
+        Vector{Float64}[
+            Float64[1, 0],
+            Float64[0, 1]
+        ],
+        # sites
+        S[
+            newSite(S, Float64[0.0,0.0], getDefaultLabelA(LS)),
+            newSite(S, Float64[0.5,0.0], getDefaultLabelB(LS)),
+            newSite(S, Float64[0.5,0.5], getDefaultLabelA(LS)),
+            newSite(S, Float64[0.0,0.5], getDefaultLabelB(LS))
+        ],
+        # bonds
+        B[
+            newBond(B, 1,2, getDefaultLabelX(LB), (-1,0)),
+            newBond(B, 1,4, getDefaultLabelY(LB), (0,-1)),
+            newBond(B, 1,2, getDefaultLabelX(LB), (0,0)),
+            newBond(B, 1,4, getDefaultLabelX(LB), (0,0)),
+
+            newBond(B, 2,1, getDefaultLabelX(LB), (1,0)),
+            newBond(B, 2,3, flipLabel(getDefaultLabelX(LB)), (0,0)),
+            newBond(B, 2,1, getDefaultLabelX(LB), (0,0)),
+            newBond(B, 2,3, flipLabel(getDefaultLabelY(LB)), (0,-1)),
+
+            newBond(B, 3,4, getDefaultLabelX(LB), (0,0)),
+            newBond(B, 3,2, flipLabel(getDefaultLabelX(LB)), (0,0)),
+            newBond(B, 3,4, getDefaultLabelY(LB), (1,0)),
+            newBond(B, 3,2, flipLabel(getDefaultLabelY(LB)), (0,1)),
+
+            newBond(B, 4,3, getDefaultLabelX(LB), (0,0)),
+            newBond(B, 4,1, getDefaultLabelY(LB), (0,1)),
+            newBond(B, 4,3, getDefaultLabelY(LB), (-1,0)),
+            newBond(B, 4,1, getDefaultLabelX(LB), (0,0)),
+        ]
+    )
+end
